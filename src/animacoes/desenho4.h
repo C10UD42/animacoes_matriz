2184ede306628c689516dc7b69e17b1502217ff2
Elisson Marques <nadsonnodachi@gmail.com>
2025-01-26 00:43:49 -0300

    feat: 🎉 separar animações em arquivos individuais
    - Criar arquivos de cabeçalho para cada animação (desenho0 a desenho9)
    - Incluir os arquivos de animação no arquivo principal animacoes_matriz.c
    - Inicializar as animações em branco para desenho2 a desenho9
    
    Melhorar a organização do código separando as animações em arquivos individuais

---
+++ src/animacoes/desenho4.h
@@ -0,0 +1,7 @@
+#ifndef DESENHO4_H
+#define DESENHO4_H
+
+double desenho4[5][25] = {
+};
+
+#endif // DESENHO4_H
---
+++ src/animacoes/desenho9.h
@@ -0,0 +1,7 @@
+#ifndef DESENHO9_H
+#define DESENHO9_H
+
+double desenho9[5][25] = {
+};
+
+#endif // DESENHO9_H
